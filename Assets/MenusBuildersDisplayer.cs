be806562b5631882c8e8d50d933545ac68516029
myrilandel <jonathan.navarro.amu@gmail.com>
2018-02-23 23:18:39 +0100

    UpdateProdScene withGui

---
+++ Assets/MenusBuildersDisplayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MenusBuildersDisplayer : ToucheableElement {
+
+    [SerializeField] protected GameObject associatedMenus;
+
+    public override void ActionOnTouch()
+    {
+        ItemDisplayer[] itemsDisplayers = associatedMenus.GetComponentsInChildren<ItemDisplayer>();
+        for(int i = 0; i < itemsDisplayers.Length; i++)
+        {
+            if(itemsDisplayers[i].IsActivated)
+            {
+                itemsDisplayers[i].DisableItem();
+            }
+        }
+        associatedMenus.SetActive(!associatedMenus.activeSelf);
+    }
+}
---
+++ Assets/Scripts/ItemDisplayer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class ItemDisplayer : ToucheableElement
+{
+
+    [SerializeField] protected GameObject chevron;
+    [SerializeField] protected Text description;
+    [SerializeField] protected GameObject descriptionPanel;
+    [SerializeField] protected GameObject GameObjectToInvoke;
+
+    public bool IsActivated = false;
+
+    protected void Start()
+    {
+        chevron.SetActive(false);
+        descriptionPanel.SetActive(false);
+        transform.parent.gameObject.SetActive(false);
+    }
+
+    public override void ActionOnTouch()
+    { 
+        if(!IsActivated)
+        {
+            EnableItem();
+        }
+        else
+        {
+            DisableItem();
+        }
+    }
+
+    protected void EnableItem()
+    {
+        ItemDisplayer[] itemDisplayers = transform.parent.GetComponentsInChildren<ItemDisplayer>();
+        for(int i = 0; i < itemDisplayers.Length; i++)
+        {
+            if(itemDisplayers[i] != this && itemDisplayers[i].IsActivated)
+            {
+                itemDisplayers[i].DisableItem();
+            }
+        }
+        IsActivated = true;
+        chevron.SetActive(true);
+        description.text = GetDescription();
+        descriptionPanel.SetActive(true);
+    }
+
+    public void DisableItem()
+    {
+        IsActivated = false;
+        chevron.SetActive(false);
+        description.text = "";
+        descriptionPanel.SetActive(false);
+    }
+
+    public GameObject GetGameObjectToInvoke()
+    {
+        return GetGameObjectToInvoke();
+    }
+
+    protected abstract string GetDescription();
+
+}
