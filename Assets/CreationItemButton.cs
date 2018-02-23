4f642e1f926a3b93c0ae11557ea152ce784c435a
myrilandel <jonathan.navarro.amu@gmail.com>
2018-02-24 00:08:06 +0100

    Updating Invoke button

---
+++ Assets/CreationItemButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreationItemButton : ToucheableElement
+{
+    protected GameObject objectToCreate;
+
+    public override void ActionOnTouch()
+    {
+        //GameManagement.instance.ObjToCreate = objectToCreate;
+    }
+
+    public void PassObjectToCreate(GameObject obj)
+    {
+        objectToCreate = obj;
+    }
+
+}
