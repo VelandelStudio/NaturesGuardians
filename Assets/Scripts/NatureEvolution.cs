9ab304e722f54f1979f9765bcdf9f00a0d8cea36
Veltouille <julienstatho@orange.fr>
2018-02-24 01:38:28 +0100

    Rescale + NatureEvolution

---
+++ Assets/Scripts/NatureEvolution.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Statistics and calculation Script for the Biome
+public class NatureEvolution : MonoBehaviour {
+
+    private int trees;
+    private int rocks;
+    private int bushes;
+
+    public void AddTree()
+    {
+        trees++;
+    }
+
+    public void AddRocks()
+    {
+        rocks++;
+    }
+
+    public void AddBushes()
+    {
+        bushes++;
+    }
+
+    public void RemoveTree()
+    {
+        trees--;
+    }
+
+    public void RemoveRocks()
+    {
+        rocks--;
+    }
+
+    public void RemoveBushes()
+    {
+        bushes--;
+    }
+}
